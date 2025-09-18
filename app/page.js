9c217ad60c5dafb6c58cc96e204ed8df4fc9bb51
tamimSteadFast <steadfast.tamim@gmail.com>
2025-09-18 10:55:15 +0600

    refactor(home): Remove Vercel logo from page
    The Vercel logo, which is part of the default Next.js starter template, has been removed from the home page.
    
    This change cleans up the user interface by removing placeholder content, preparing the page for actual application components.

--- app/page.js
+++ app/page.js
@@ -3,8 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
  <div>
-    <h1>Home</h1>
-    <Image src="/vercel.svg" alt="Vercel Logo" width={100} height={24} />
+    <h1>Home</h1> 
  </div>
   );
 }
