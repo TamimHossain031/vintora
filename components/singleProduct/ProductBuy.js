882a1470eefce4cf4e0b4dc715bbb21c0a1d77e2
tamimSteadFast <steadfast.tamim@gmail.com>
2025-10-07 16:29:27 +0600

    fix(ProductBuy): update product name and price display for clarity

--- components/singleProduct/ProductBuy.js
+++ components/singleProduct/ProductBuy.js
@@ -2,7 +2,11 @@
 export default function ProductBuy() {
   return (
     <div className="w-1/2 h-fit bg-[#f0f0f0] sticky top-[150px] p-5 rounded">
-      <p className="text-xl font-semibold">tamim</p>
+      <h2>product name</h2>
+       <p>$120</p>
+       <div className="border-t border">
+
+       </div>
     </div>
   );
 }
